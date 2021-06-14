0de25635459bcc24ada1fd6396f17d5218f145e1
SallyLueNoa <sally.lue.noa@gmail.com>
2021-06-14 22:23:00 +0900

    Add test code #3
    * Delete Activity.kt because startActivityForResult is deprecated

--- frextensions/src/main/java/org/fog_rock/frextensions/android/Activity.kt
+++
@@ -1,7 +0,0 @@
-package org.fog_rock.frextensions.android
-
-import android.app.Activity
-
-inline fun <reified T: Activity> Activity.startActivityForResult(requestCode: Int) {
-    startActivityForResult(newIntent<T>(), requestCode)
-}
