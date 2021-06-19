ede7ef155880ee5f7eca04d590d41f5e73fbca6a
SallyLueNoa <sally.lue.noa@gmail.com>
2021-06-19 23:46:56 +0900

    Add test code #3
    * Replace Truth library in BundleTest and IntentTest.

--- frextensions/src/androidTest/java/org/fog_rock/frextensions/android/IntentTest.kt
+++ frextensions/src/androidTest/java/org/fog_rock/frextensions/android/IntentTest.kt
@@ -2,7 +2,7 @@ package org.fog_rock.frextensions.android
 
 import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.*
+import com.google.common.truth.Truth
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,7 +18,7 @@ class IntentTest {
     @Test fun arrayExtra_normal() {
         val intent = Intent().apply { putArrayExtra("array", stringArray) }
         val array = intent.getArrayExtra<String>("array")
-        assertArrayEquals(stringArray, array)
+        Truth.assertThat(array).isEqualTo(stringArray)
     }
     /**
      * Intent.putArrayExtra(), getArrayExtra(): 異常系、テンプレートタイプミス
@@ -26,7 +26,7 @@ class IntentTest {
     @Test fun arrayExtra_tempInvalid() {
         val intent = Intent().apply { putArrayExtra("array", stringArray) }
         val array = intent.getArrayExtra<Int>("array")
-        assertNull(array)
+        Truth.assertThat(array).isNull()
     }
     /**
      * Intent.putArrayExtra(), getArrayExtra(): 異常系、putタイプミス
@@ -34,7 +34,7 @@ class IntentTest {
     @Test fun arrayExtra_putInvalid() {
         val intent = Intent().apply { putExtra("number", 1) }
         val array = intent.getArrayExtra<String>("number")
-        assertNull(array)
+        Truth.assertThat(array).isNull()
     }
     /**
      * Intent.putArrayExtra(), getArrayExtra(): 異常系、getタイプミス
@@ -42,7 +42,7 @@ class IntentTest {
     @Test fun arrayExtra_getInvalid() {
         val intent = Intent().apply { putArrayExtra("array", stringArray) }
         val number = intent.getIntExtra("array", 1)
-        assertEquals(1, number)
+        Truth.assertThat(number).isEqualTo(1)
     }
     /**
      * Intent.putArrayExtra(), getArrayExtra(): 異常系、put未指定
@@ -50,7 +50,7 @@ class IntentTest {
     @Test fun arrayExtra_empty() {
         val intent = Intent()
         val array = intent.getArrayExtra<String>("array")
-        assertNull(array)
+        Truth.assertThat(array).isNull()
     }
 
     /**
@@ -59,7 +59,7 @@ class IntentTest {
     @Test fun listExtra_normal() {
         val intent = Intent().apply { putListExtra("list", stringList) }
         val list = intent.getListExtra<String>("list")
-        assertEquals(stringList, list)
+        Truth.assertThat(list).isEqualTo(stringList)
     }
     /**
      * Intent.putListExtra(), getListExtra(): 異常系、テンプレートタイプミス
@@ -67,7 +67,7 @@ class IntentTest {
     @Test fun listExtra_tempInvalid() {
         val intent = Intent().apply { putListExtra("array", stringList) }
         val array = intent.getListExtra<Int>("array")
-        assertNull(array)
+        Truth.assertThat(array).isNull()
     }
     /**
      * Intent.putListExtra(), getListExtra(): 異常系、putタイプミス
@@ -75,7 +75,7 @@ class IntentTest {
     @Test fun listExtra_putInvalid() {
         val intent = Intent().apply { putExtra("number", 1) }
         val array = intent.getListExtra<String>("number")
-        assertNull(array)
+        Truth.assertThat(array).isNull()
     }
     /**
      * Intent.putListExtra(), getListExtra(): 異常系、getタイプミス
@@ -83,7 +83,7 @@ class IntentTest {
     @Test fun listExtra_getInvalid() {
         val intent = Intent().apply { putListExtra("list", stringList) }
         val number = intent.getIntExtra("list", 1)
-        assertEquals(1, number)
+        Truth.assertThat(number).isEqualTo(1)
     }
     /**
      * Intent.putListExtra(), getListExtra(): 異常系、put未指定
@@ -91,6 +91,6 @@ class IntentTest {
     @Test fun listExtra_empty() {
         val intent = Intent()
         val list = intent.getListExtra<String>("list")
-        assertNull(list)
+        Truth.assertThat(list).isNull()
     }
 }
