877e7ee4f1d422fa0d91f8db349855579e0b71b2
SallyLueNoa <sally.lue.noa@gmail.com>
2021-06-19 21:18:46 +0900

    Add test code #3
    * Replace Truth library in DateTest

--- frextensions/src/test/java/org/fog_rock/frextensions/kotlin/DateTest.kt
+++ frextensions/src/test/java/org/fog_rock/frextensions/kotlin/DateTest.kt
@@ -1,7 +1,6 @@
 package org.fog_rock.frextensions.kotlin
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
+import com.google.common.truth.Truth
 import org.junit.Test
 import java.util.*
 
@@ -10,116 +9,118 @@ class DateTest {
     // 2021/06/01 04:23:57.558 JST
     // 2021/05/31 19:23:57.558 UTC
     private val dateLong = 1622489037558L
+    private val date = Date(dateLong)
 
     // Cutoff milli seconds.
     private val dateLongCutMillis = 1622489037000L
+    private val dateCutMillis = Date(dateLongCutMillis)
 
     /**
      * Long.toDefaultDateString(): 正常系
      */
     @Test fun toDefaultDateString_default() {
-        val dateString = dateLong.toDefaultDateString(FORMAT_YYYYMMDD_HHMMSS_SSS)
-        assertEquals("2021/06/01 04:23:57.558", dateString)
+        Truth.assertThat(dateLong.toDefaultDateString(FORMAT_YYYYMMDD_HHMMSS_SSS))
+            .isEqualTo("2021/06/01 04:23:57.558")
     }
     /**
      * Long.toDefaultDateString(): 正常系、日本語ロケール
      */
     @Test fun toDefaultDateString_localeJPN() {
-        val dateString = dateLong.toDefaultDateString(FORMAT_JP_YYYYMMDDE_HHMMSS, Locale.JAPAN)
-        assertEquals("2021年6月1日（火）4時23分57秒", dateString)
+        Truth.assertThat(dateLong
+            .toDefaultDateString(FORMAT_JP_YYYYMMDDE_HHMMSS, Locale.JAPAN))
+            .isEqualTo("2021年6月1日（火）4時23分57秒")
     }
 
     /**
      * Long.toUtcDateString(): 正常系
      */
     @Test fun toUtcDateString_default() {
-        val dateString = dateLong.toUtcDateString()
-        assertEquals("2021-05-31T19:23:57.558Z", dateString)
+        Truth.assertThat(dateLong.toUtcDateString())
+            .isEqualTo("2021-05-31T19:23:57.558Z")
     }
     /**
      * Long.toUtcDateString(): 正常系、別フォーマット
      */
     @Test fun toUtcDateString_format() {
-        val dateString = dateLong.toUtcDateString(FORMAT_YYYYMMDD_HHMMSS)
-        assertEquals("2021/05/31 19:23:57", dateString)
+        Truth.assertThat(dateLong.toUtcDateString(FORMAT_YYYYMMDD_HHMMSS))
+            .isEqualTo("2021/05/31 19:23:57")
     }
 
     /**
      * Long.toDateString(): 正常系
      */
     @Test fun toDateString_variety() {
-        val dateString = dateLong.toDateString(
-            "EEE, M/d/yyyy HH:mm:ss", Locale.US, TimeZone.getTimeZone("America/Anguilla"))
-        assertEquals("Mon, 5/31/2021 15:23:57", dateString)
+        Truth.assertThat(dateLong
+            .toDateString("EEE, M/d/yyyy HH:mm:ss", Locale.US,
+                TimeZone.getTimeZone("America/Anguilla")))
+            .isEqualTo("Mon, 5/31/2021 15:23:57")
     }
     /**
      * Long.toDateString(): 異常系、タイムゾーンミス
      */
     @Test fun toDateString_wrongTimeZone() {
-        val dateString = dateLong.toDateString(
-            "EEE, M/d/yyyy HH:mm:ss", Locale.US, TimeZone.getTimeZone("Invalid"))
-        assertEquals("Mon, 5/31/2021 19:23:57", dateString)
+        Truth.assertThat(dateLong
+            .toDateString("EEE, M/d/yyyy HH:mm:ss", Locale.US,
+                TimeZone.getTimeZone("Invalid")))
+            .isEqualTo("Mon, 5/31/2021 19:23:57")
     }
 
     /**
      * String.defaultToDate(): 正常系
      */
     @Test fun defaultToDate_default() {
-        val dateString = "2021/06/01 04:23:57.558"
-        val date = dateString.defaultToDate(FORMAT_YYYYMMDD_HHMMSS_SSS)
-        assertEquals(dateLong, date?.time)
+        Truth.assertThat("2021/06/01 04:23:57.558".defaultToDate(FORMAT_YYYYMMDD_HHMMSS_SSS))
+            .isEqualTo(date)
     }
     /**
      * String.defaultToDate(): 正常系、日本語ロケール
      */
     @Test fun defaultToDate_localeJPN() {
-        val dateString = "2021年6月1日（火）4時23分57秒"
-        val date = dateString.defaultToDate(FORMAT_JP_YYYYMMDDE_HHMMSS, Locale.JAPAN)
-        assertEquals(dateLongCutMillis, date?.time)
+        Truth.assertThat("2021年6月1日（火）4時23分57秒"
+            .defaultToDate(FORMAT_JP_YYYYMMDDE_HHMMSS, Locale.JAPAN))
+            .isEqualTo(dateCutMillis)
     }
 
     /**
      * String.utcToDate(): 正常系
      */
     @Test fun utcToDate_default() {
-        val dateString = "2021-05-31T19:23:57.558Z"
-        val date = dateString.utcToDate()
-        assertEquals(dateLong, date?.time)
+        Truth.assertThat("2021-05-31T19:23:57.558Z".utcToDate())
+            .isEqualTo(date)
     }
     /**
      * String.utcToDate(): 正常系、別フォーマット
      */
     @Test fun utcToDate_format() {
-        val dateString = "2021/05/31 19:23:57"
-        val date = dateString.utcToDate(FORMAT_YYYYMMDD_HHMMSS)
-        assertEquals(dateLongCutMillis, date?.time)
+        Truth.assertThat("2021/05/31 19:23:57".utcToDate(FORMAT_YYYYMMDD_HHMMSS))
+            .isEqualTo(dateCutMillis)
     }
 
     /**
      * String.toDate(): 正常系
      */
     @Test fun toDate_variety() {
-        val dateString = "Mon, 5/31/2021 15:23:57"
-        val date = dateString.toDate(
-            "EEE, M/d/yyyy HH:mm:ss", Locale.US, TimeZone.getTimeZone("America/Anguilla"))
-        assertEquals(dateLongCutMillis, date?.time)
+        Truth.assertThat("Mon, 5/31/2021 15:23:57"
+            .toDate("EEE, M/d/yyyy HH:mm:ss", Locale.US,
+                TimeZone.getTimeZone("America/Anguilla")))
+            .isEqualTo(dateCutMillis)
     }
     /**
      * String.toDate(): 異常系、フォーマットミス
      */
     @Test fun toDate_wrongFormat() {
-        val dateString = "Mon, 5/31/2021 15:23:57"
-        val date = dateString.toDate(
-            "M/d/yyyy HH:mm:ss", Locale.US, TimeZone.getTimeZone("America/Anguilla"))
-        assertNull(date)
+        Truth.assertThat("Mon, 5/31/2021 15:23:57"
+            .toDate("M/d/yyyy HH:mm:ss", Locale.US,
+                TimeZone.getTimeZone("America/Anguilla")))
+            .isNull()
     }
     /**
      * String.toDate(): 異常系、タイムゾーンミス
      */
     @Test fun toDate_wrongTimeZone() {
-        val dateString = "Mon, 5/31/2021 19:23:57"
-        val date = dateString.toDate(
-            "EEE, M/d/yyyy HH:mm:ss", Locale.US, TimeZone.getTimeZone("Invalid"))
-        assertEquals(dateLongCutMillis, date?.time)
+        Truth.assertThat("Mon, 5/31/2021 19:23:57"
+            .toDate("EEE, M/d/yyyy HH:mm:ss", Locale.US,
+                TimeZone.getTimeZone("Invalid")))
+            .isEqualTo(dateCutMillis)
     }
 }
