6a24da30433fee62bab19555891b34119eff3642
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-07 23:42:33 +0900

    [Feature implementation] Add logical operations on multiple flags #25
    * Add KDoc

--- frextensions/src/main/java/org/fog_rock/frextensions/ktx/operation/Operation.kt
+++ frextensions/src/main/java/org/fog_rock/frextensions/ktx/operation/Operation.kt
@@ -1,5 +1,12 @@
 package org.fog_rock.frextensions.ktx.operation
 
+/**
+ * Performs logical `and` operations on multiple boolean values.
+ * If a part of the values is null, it is ignored to performs the operations.
+ * If all values are null or no value, the result is null.
+ * @param flags Variable number of boolean values
+ * @return A result of logical `and` operations, or Null if all values are null or no value.
+ */
 fun andAll(vararg flags: Boolean?): Boolean? {
     var ret: Boolean? = null
     flags.forEach {
@@ -8,6 +15,13 @@ fun andAll(vararg flags: Boolean?): Boolean? {
     return ret
 }
 
+/**
+ * Performs logical `or` operations on multiple boolean values.
+ * If a part of the values is null, it is ignored to performs the operations.
+ * If all values are null or no value, the result is null.
+ * @param flags Variable number of boolean values
+ * @return A result of logical `or` operations, or Null if all values are null or no value.
+ */
 fun orAll(vararg flags: Boolean?): Boolean? {
     var ret: Boolean? = null
     flags.forEach {
@@ -16,6 +30,13 @@ fun orAll(vararg flags: Boolean?): Boolean? {
     return ret
 }
 
+/**
+ * Performs logical `xor` operations on multiple boolean values.
+ * If a part of the values is null, it is ignored to performs the operations.
+ * If all values are null or no value, the result is null.
+ * @param flags Variable number of boolean values
+ * @return A result of logical `xor` operations, or Null if all values are null or no value.
+ */
 fun xorAll(vararg flags: Boolean?): Boolean? {
     var ret: Boolean? = null
     flags.forEach {
