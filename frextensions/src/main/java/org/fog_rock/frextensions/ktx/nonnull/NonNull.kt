b0964f62b820b21699d93e619b543fe0aedb50c9
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-09 22:52:27 +0900

    [Feature implementation] Return a default value if the nullable value is null #27
    * Rename method names. (nonNull -> toNonNull)

--- frextensions/src/main/java/org/fog_rock/frextensions/ktx/nonnull/NonNull.kt
+++ frextensions/src/main/java/org/fog_rock/frextensions/ktx/nonnull/NonNull.kt
@@ -7,64 +7,64 @@ package org.fog_rock.frextensions.ktx.nonnull
  * @param default A default value
  * @return A non-null value itself, or [default] if the value itself is null.
  */
-inline fun <reified T> T?.nonNull(default: T): T = this ?: default
+inline fun <reified T> T?.toNonNull(default: T): T = this ?: default
 
 /**
  * Get a non-null boolean value itself for the nullable boolean value.
  * @return A non-null boolean value itself, or false if the value itself is null.
  */
-fun Boolean?.nonNull(): Boolean = nonNull(false)
+fun Boolean?.toNonNull(): Boolean = toNonNull(false)
 
 /**
  * Get a non-null integer value itself for the nullable integer value.
  * @return A non-null integer value itself, or zero if the value itself is null.
  */
-fun Int?.nonNull(): Int = nonNull(0)
+fun Int?.toNonNull(): Int = toNonNull(0)
 
 /**
  * Get a non-null long value itself for the nullable long value.
  * @return A non-null long value itself, or zero if the value itself is null.
  */
-fun Long?.nonNull(): Long = nonNull(0L)
+fun Long?.toNonNull(): Long = toNonNull(0L)
 
 /**
  * Get a non-null float value itself for the nullable float value.
  * @return A non-null float value itself, or zero if the value itself is null.
  */
-fun Float?.nonNull(): Float = nonNull(0.0f)
+fun Float?.toNonNull(): Float = toNonNull(0.0f)
 
 /**
  * Get a non-null double value itself for the nullable double value.
  * @return A non-null double value itself, or zero if the value itself is null.
  */
-fun Double?.nonNull(): Double = nonNull(0.0)
+fun Double?.toNonNull(): Double = toNonNull(0.0)
 
 /**
  * Get a non-null string value itself for the nullable string value.
  * @return A non-null string value itself, or an empty string if the value itself is null.
  */
-fun String?.nonNull(): String = nonNull("")
+fun String?.toNonNull(): String = toNonNull("")
 
 /**
  * Get a non-null array itself for the nullable array.
  * @return A non-null array itself, or an empty array if the array itself is null.
  */
-inline fun <reified T> Array<T>?.nonNull(): Array<T> = nonNull(emptyArray())
+inline fun <reified T> Array<T>?.toNonNull(): Array<T> = toNonNull(emptyArray())
 
 /**
  * Get a non-null list itself for the nullable list.
  * @return A non-null list itself, or an empty list if the list itself is null.
  */
-inline fun <reified E> List<E>?.nonNull(): List<E> = nonNull(emptyList())
+inline fun <reified E> List<E>?.toNonNull(): List<E> = toNonNull(emptyList())
 
 /**
  * Get a non-null set itself for the nullable set.
  * @return A non-null set itself, or an empty set if the set itself is null.
  */
-inline fun <reified E> Set<E>?.nonNull(): Set<E> = nonNull(emptySet())
+inline fun <reified E> Set<E>?.toNonNull(): Set<E> = toNonNull(emptySet())
 
 /**
  * Get a non-null map itself for the nullable map.
  * @return A non-null map itself, or an empty map if the map itself is null.
  */
-inline fun <reified K, reified V> Map<K, V>?.nonNull(): Map<K, V> = nonNull(emptyMap())
+inline fun <reified K, reified V> Map<K, V>?.toNonNull(): Map<K, V> = toNonNull(emptyMap())
