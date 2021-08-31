0ff8b94fbdadaf90d406aa552125e400ff363340
SallyLueNoa <sally.lue.noa@gmail.com>
2021-08-31 23:42:15 +0900

    Remove FRFragmentCallback from the project #31
    * Modify sample codes and test codes.

--- sample/src/main/java/org/fog_rock/frextensionssample/SubFragment.kt
+++ sample/src/main/java/org/fog_rock/frextensionssample/SubFragment.kt
@@ -5,26 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.fog_rock.frextensions.androidx.fragment.FRFragmentCallback
-import org.fog_rock.frextensions.androidx.fragment.activityCallback
-import org.fog_rock.frextensions.androidx.log.TAG
-import org.fog_rock.frextensionssample.databinding.FragmentDefaultBinding
+import org.fog_rock.frextensionssample.databinding.FragmentSubBinding
 
 class SubFragment : Fragment() {
 
-    private var _binding: FragmentDefaultBinding? = null
+    private var _binding: FragmentSubBinding? = null
     private val binding get() = _binding!!
-
-    private val callback: FRFragmentCallback? by lazy { activityCallback() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentDefaultBinding.inflate(inflater, container, false)
-        binding.textViewClassName.text = TAG
-        callback?.onCreateFragmentView(TAG)
+        _binding = FragmentSubBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
