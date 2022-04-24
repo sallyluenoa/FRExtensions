62ba875df9a59cf9c212266e023ad02c35b82e10
SallyLueNoa <sally.lue.noa@gmail.com>
2022-04-24 16:39:17 +0900

    [Feature implementation] Convert from a value of any type to an Enum class. #36
    * Refactoring: Add Kdoc.

--- frextensions/src/main/java/org/fog_rock/frextensions/ktx/enum/FRBaseEnum.kt
+++ frextensions/src/main/java/org/fog_rock/frextensions/ktx/enum/FRBaseEnum.kt
@@ -4,5 +4,8 @@ package org.fog_rock.frextensions.ktx.enum
  * A interface for converting from a value of any type to an Enum class.
  */
 interface FRBaseEnum<T> {
+    /**
+     * A value of any type
+     */
     val value: T
 }
