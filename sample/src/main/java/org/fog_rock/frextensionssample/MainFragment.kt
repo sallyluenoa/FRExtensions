5bca43153f232ec514ff23366ba71863cf8d71be
SallyLueNoa <sally.lue.noa@gmail.com>
2022-04-25 23:13:13 +0900

    [Feature implementation] Apply the Material Design to the Sample Code #38
    * Apply the Material Design to Fragments.

--- sample/src/main/java/org/fog_rock/frextensionssample/MainFragment.kt
+++ sample/src/main/java/org/fog_rock/frextensionssample/MainFragment.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import org.fog_rock.frextensionssample.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+
+    companion object {
+        private const val ARGS_COUNT = "count"
+        private const val DEFAULT_VALUE = 0
+
+        fun newInstance(count: Int = DEFAULT_VALUE): MainFragment =
+            MainFragment().apply {
+                arguments = bundleOf(ARGS_COUNT to count)
+            }
+    }
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -16,8 +27,12 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View = run {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
+
+        val count = arguments?.getInt(ARGS_COUNT, DEFAULT_VALUE) ?: DEFAULT_VALUE
+        binding.textViewMessage.text = getString(R.string.label_textView_mainFragment, count)
+
+        binding.root
     }
 }
