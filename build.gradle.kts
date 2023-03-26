643bf15ad1f5334ea4bf726589ade37c6a390228
SallyLueNoa <sally.lue.noa@gmail.com>
2023-03-26 20:10:37 +0900

    [Maintenance] Update Android Studio version. #34
    * Update versions of Gradle, Kotlin, SDKs, and libraries.
    * Renew structures of Gradle.
    * Add version catalogs for libraries and plugins.
    * Change Gradle language from Groovy to Kotlin. (.gradle -> .kts)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,13 @@
  * limitations under the License.
  */
 
-include ':frextensions'
-include ':sample'
-rootProject.name = "FRExtensions"
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.app.gradle) apply false
+    alias(libs.plugins.android.lib.gradle) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.dokka) apply false
+}
+
+// Workaround for "Expecting an expression" build error
+println("")
